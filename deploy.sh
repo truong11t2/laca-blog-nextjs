32bb5fe832ecfa67d8220d552997f97aa6497acb
truong11t2 <truong11t2@gmail.com>
2024-04-10 07:11:21 +0700

    adjust script for new image

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
-cd ./deploy
-docker compose down
-docker image rm truong11t2/laca-blog-nextjs:1.0.4
-docker network rm laca-net
+# cd ./deploy
+# docker compose down
+# docker image rm truong11t2/laca-blog-nextjs:1.0.4
+# docker network rm laca-net
 
-docker image pull truong11t2/laca-blog-nextjs:1.0.4
-docker compose up
+# docker image pull truong11t2/laca-blog-nextjs:1.0.4
+# docker compose up
+
+docker build -t laca-blog-nextjs .
+docker tag laca-blog-nextjs:latest truong11t2/laca-blog-nextjs:1.0.5
+docker push truong11t2/laca-blog-nextjs:1.0.5
+
+
+ssh root@192.227.234.198 "sh ./deploy.sh"
